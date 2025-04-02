548d4e9cf5dcb44e628efefb6d982767d8cb50d4
jhsong <jh@song>
2025-04-02 15:08:35 +0900

    add  Modal

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -3,9 +3,14 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import { signIn, useSession, signOut } from "next-auth/react";
-
+import Modal from "react-modal";
+import { useState } from "react";
+import { IoMdAddCircleOutline } from "react-icons/io";
+import { HiCamera } from "react-icons/hi";
+import { AiOutlineClose } from "react-icons/ai";
 export default function Header() {
   const { data: session } = useSession();
+  const [isOpen, setIsOpen] = useState(false);
   console.log(session);
   return (
     <div className="shadow-sm border-b sticky top-0 bg-white z-30 p-3">
@@ -23,12 +28,20 @@ export default function Header() {
         />
 
         {session ? (
-          <img
-            src={session.user.image}
-            alt={session.user.name}
-            className="h-10 w-10 rounded-full cursor-pointer"
-            onClick={signOut}
-          />
+          <>
+            <div className="flex gap-2 items-center">
+              <IoMdAddCircleOutline
+                className="text-2xl cursor-pointer transform hover:scale-125 transition duration-300 hover:text-red-600"
+                onClick={() => setIsOpen(true)}
+              />
+              <img
+                src={session.user.image}
+                alt={session.user.name}
+                className="h-10 w-10 rounded-full cursor-pointer"
+                onClick={signOut}
+              />
+            </div>
+          </>
         ) : (
           <button
             onClick={() => signIn}
@@ -38,6 +51,33 @@ export default function Header() {
           </button>
         )}
       </div>
+
+      {isOpen && (
+        <Modal
+          isOpen={isOpen}
+          ariaHideApp={false}
+          className="max-w-lg w-[90%] p-6 absolute top-56 left-[50%] translate-x-[-50%] bg-white border-2 rounded-md shadow-md"
+          onRequestClose={() => setIsOpen(false)}
+        >
+          <div className="flex flex-col justify-center items-center h-[100%]">
+            <HiCamera className="text-4xl text-gray-400 cursor-pointer" />
+            <button onClick={() => setIsOpen(false)}>Close</button>
+          </div>
+          <input
+            type="text"
+            maxLength="150"
+            placeholder="Please enter you caption..."
+            className="m-4 border text-center w-full focus:ring-0 outline-none"
+          />
+          <button className="w-full bg-red-600 text-white p-2 shadow-md rounded-lg hover:brightness-105 disabled:bg-gray-200 disabled:cursor-not-allowed disabled:hover:brightness-100">
+            Upload Post
+          </button>
+          <AiOutlineClose
+            className="cursor-pointer absolute top-2 right-2 hover:text-red-600 transition duration-300"
+            onClick={() => setIsOpen(false)}
+          />
+        </Modal>
+      )}
     </div>
   );
 }
